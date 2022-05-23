e7f93c8c642243355ed5801499676abe2f9b764d
AntonArk <anton.ark80@gmail.com>
2022-05-23 13:03:01 +0400

    Add Barbers and colour styles

--- app.rb
+++ app.rb
@@ -13,3 +13,26 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+end
